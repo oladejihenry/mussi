87b7f0506403cd3fa9ca7d3bcd93ef3eb6db094b
Henry Oladeji <henryoladeji@Henrys-MacBook-Pro.local>
2021-03-22 04:36:37 +0000

    Minor Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $title = "Welcome to Musigent";
+        $title = 'Welcome to Musigent';
         $post = Post::latest()->paginate(6);
         return view('pages.welcome', compact('post','title'));
     }
